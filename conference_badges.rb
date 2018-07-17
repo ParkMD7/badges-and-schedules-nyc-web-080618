ea7ad77e6c66d57834c03ec55d008834dc6e6de3
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 19:28:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+  attendees.each do |name|
+   puts "Hello, my name is #{name}."
+  end
+end
+
+  
+end
+
+
+def assign_rooms
+  
+end
+
+
+def printer
+  
+end
