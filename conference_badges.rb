07faa419fda8138d3c7548d8f3a4f865766f2a3a
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 19:39:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def batch_badge_creator(attendees)
   attendees.each do |attendee|
     attendees << badge_maker(name)
-   return "Hello, my name is #{attendee}."
+   return "Hello, my name is #{attendees}."
   end
 end
 
